a3d449e0d7e745d8c9d12333a5f65cd8f2575c2a
ibrahim gul <kufluibrahim@gmail.com>
2025-07-07 19:17:43 +0100

    simple gpio but not working

--- 03_gpioctrl/gpioctrl.c
+++ 03_gpioctrl/gpioctrl.c
@@ -1,10 +1,40 @@
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/gpio/consumer.h>
+
+static struct gpiod_desc *led, *button;
+
+#define IO_LED 21
+#define IO_BUTTON 20
+
+#define IO_OFFSET 0
 
 
 static int __init my_init(void)
 {
-	printk("hello - Hello, Kernel!\n");
+	int status;
+	led = gpio_to_desc(IO_LED +IO_OFFSET);
+	if(!led){
+		printk("gpioctrl - Error getting pin21\n");
+		return -ENODEV;
+	}
+	button = go_to_desc(IO_BUTTON+IO_OFFSET);
+	if(!button){
+		printk("gpioctrl - Error getting pin20\n");
+		return -ENODEV;
+	}
+	status = gpiod_direction_output(led, 0);
+	if(status){
+		printk("gpioctrl - Error setting pin20 to output\n");
+		return status;
+	}
+	status = gpiod_direction_input(led, 1);
+	if(status){
+		printk("gpioctrl - Error setting pin21 to input");
+		return status;
+	}
+	gpiod_set_value(led, 1);
+	printk("button is %spressed\n", gpiod_get_value(button) ? "" : "not ");
 	return 0;
 }
 
@@ -18,4 +48,4 @@ module_exit(my_exit);
 
 MODULE_LICENSE("CAL");
 MODULE_AUTHOR("h4xrunner");
-MODULE_DESCRIPTION("A simple Hello Word Linux Kernel module");
+MODULE_DESCRIPTION("An example for using GPIOs without the device tree");
