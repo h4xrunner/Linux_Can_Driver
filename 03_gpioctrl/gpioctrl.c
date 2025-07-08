b0198fca83ae1e88d9cb2887473d0854cc362614
ibrahim gul <kufluibrahim@gmail.com>
2025-07-08 00:02:28 +0000

    syntax fixed

--- 03_gpioctrl/gpioctrl.c
+++ 03_gpioctrl/gpioctrl.c
@@ -6,46 +6,50 @@ static struct gpio_desc *led, *button;
 
 #define IO_LED 21
 #define IO_BUTTON 20
-
 #define IO_OFFSET 0
-
 
 static int __init my_init(void)
 {
 	int status;
-	led = gpio_to_desc(IO_LED +IO_OFFSET);
-	if(!led){
-		printk("gpioctrl - Error getting pin21\n");
+
+	led = gpio_to_desc(IO_LED + IO_OFFSET);
+	if (!led) {
+		printk("gpioctrl - Error getting pin %d\n", IO_LED);
 		return -ENODEV;
 	}
-	button = gpiod_to_desc(IO_BUTTON+IO_OFFSET);
-	if(!button){
-		printk("gpioctrl - Error getting pin20\n");
+
+	button = gpio_to_desc(IO_BUTTON + IO_OFFSET);
+	if (!button) {
+		printk("gpioctrl - Error getting pin %d\n", IO_BUTTON);
 		return -ENODEV;
 	}
+
 	status = gpiod_direction_output(led, 0);
-	if(status){
-		printk("gpioctrl - Error setting pin20 to output\n");
+	if (status) {
+		printk("gpioctrl - Error setting LED to output\n");
 		return status;
 	}
-	status = gpiod_direction_input(led, 1);
-	if(status){
-		printk("gpioctrl - Error setting pin21 to input");
+
+	status = gpiod_direction_input(button);
+	if (status) {
+		printk("gpioctrl - Error setting BUTTON to input\n");
 		return status;
 	}
+
 	gpiod_set_value(led, 1);
 	printk("button is %spressed\n", gpiod_get_value(button) ? "" : "not ");
+
 	return 0;
 }
 
 static void __exit my_exit(void)
 {
-	printk("hello - Goodbye, Kernel!\n");
+	printk("gpioctrl - Module unloaded\n");
 }
 
 module_init(my_init);
 module_exit(my_exit);
 
-MODULE_LICENSE("CAL");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("h4xrunner");
-MODULE_DESCRIPTION("An example for using GPIOs without the device tree");
+MODULE_DESCRIPTION("Example GPIO kernel module without Device Tree");
