627d3ea345e4bd6d377bef2a4b19795675061d87
ibrahim gul <kufluibrahim@gmail.com>
2025-07-19 15:24:26 +0100

    syntax fixed 0 != O

--- 07_open_release_dev_cdev/test.c
+++ 07_open_release_dev_cdev/test.c
@@ -8,21 +8,21 @@ int main(int argc, char **argv){
 		printf("I need the file to open as an argument!\n");
 		return 0;
 	}
-	fd = open(argv[1], 0_RDONLY);
+	fd = open(argv[1], O_RDONLY);
 	if(fd < 0){
 		perror("open");
 		return fd;
 	}
 	close (fd); 
 	
-	fd = open(argv[1], 0_RDWR | O_SYNC);
+	fd = open(argv[1], O_RDWR | O_SYNC);
 	if(fd < 0){
 		perror("open");
 		return fd;
 	}
 	close(fd);
 	
-	fd = open(argv[1], 0_WRONLY | O_NONBLOCK);
+	fd = open(argv[1], O_WRONLY | O_NONBLOCK);
 	if(fd < 0){
 		perror("open");
 		return fd;
