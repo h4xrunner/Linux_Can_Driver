dcb2b15d7ae8ec1a79cc948bbeaa5cbfcf7e0571
h4xrunner <kufluibrahim@gmail.com>
2025-07-24 20:13:01 +0300

    some syntax error fixed on laptop

--- serdev/serdev_echo.c
+++ serdev/serdev_echo.c
@@ -33,7 +33,7 @@ static struct serdev_device_driver serdev_echo_driver = {
 
 static int serdev_echo_recv(struct serdev_device *serdev, const unsigned char *buffer, size_t size){
 	printk("serdev_echo - Received %ld bytes with \"\"\n", size, buffer);
-	return serdev_device_write_buf(struct serdev_device *serdev);
+	return serdev_device_write_buf(serdev, buffer, size);
 }
 
 static const struct serdev_device_ops serdev_echo_ops{
@@ -56,6 +56,8 @@ static int serdev_echo_probe(struct serdev_device *serdev) {
 	serdev_device_set_baudrate(serdev, 9600);
 	serdev_device_set_flow_control(serdev, false);
 	serdev_device_set_parity(serdev, SERDEV_PARITY_NONE);
+
+	status = serdev_device_write_buf(serdev, "Type something: ", sizeof("Type something "));
 	return 0;
 }
 
